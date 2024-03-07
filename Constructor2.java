7667d27be87f9149d6ef98858c25314ce1b76143
piyush021pr <piyush021pr@gmail.com>
2024-03-07 15:13:37 +0530

    ..

---
+++ Constructor2.java
@@ -0,0 +1,10 @@
+class Bike1{
+    public Bike1(){
+        System.out.println("Hii This is a bike");
+    }
+}
+public class Constructor2 {
+    public static void main(String[] args) {
+        Bike1 b=new Bike1();
+    }
+}
---
+++ Parametrized_constructor.java
@@ -0,0 +1,2 @@
+public class Parametrized_constructor {
+}
